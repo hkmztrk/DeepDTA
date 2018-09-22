9120e0ad50cff447568be2e4a92219bbd4e97cb5
hkmztrk <hkm.ozturk@gmail.com>
2018-09-22 23:02:30 +0300

    .sh file included

---
+++ source/go.sh
@@ -0,0 +1,10 @@
+python run_experiments.py --num_windows 32 \
+                          --seq_window_lengths 8 12 \
+                          --smi_window_lengths 4 8 \
+                          --batch_size 256 \
+                          --num_epoch 100 \
+                          --max_seq_len 1000 \
+                          --max_smi_len 100 \
+                          --dataset_path 'data/kiba/' \
+                          --problem_type 1 \
+                          --log_dir 'logs/'
